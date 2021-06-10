b758531d051a1c2096071e1e5a88d81565a8fd40
Chris-Eminence <emichris380@gmail.com>
2021-06-10 12:47:39 +0100

    commits changes

--- app/src/main/java/com/example/contactapp/Login.kt
+++ app/src/main/java/com/example/contactapp/Login.kt
@@ -1,11 +1,42 @@
 package com.example.contactapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.contactapp.entity.NoteEntity
+import com.example.contactapp.entity.NoteRepository
 
 class Login : AppCompatActivity() {
+
+    lateinit var emailAddressLoginEditText: EditText
+    lateinit var passwordLoginEditText: EditText
+    lateinit var loginButton: Button
+    lateinit var noteRepository: NoteRepository
+    lateinit var noteEntity: NoteEntity
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        emailAddressLoginEditText = findViewById(R.id.email_login_edit_text)
+        passwordLoginEditText = findViewById(R.id.login_password)
+        loginButton = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            if (noteEntity.emailAddressID == emailAddressLoginEditText.text.toString() && noteEntity.passwordDescription == passwordLoginEditText.text.toString()){
+                intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Account not found", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+
+    fun authenticateCredentials() {
     }
 }
--- app/src/main/java/com/example/contactapp/SignUp.kt
+++ app/src/main/java/com/example/contactapp/SignUp.kt
@@ -1,25 +1,28 @@
 package com.example.contactapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.contactapp.entity.NoteEntity
 import com.example.contactapp.entity.NoteRepository
 
 class SignUp : AppCompatActivity() {
-    lateinit var noteRepository : NoteRepository
-    lateinit var emailAddressEditText : EditText
-    lateinit var confirmedPasswordEditText : EditText
-    lateinit var initialPasswordET : EditText
-    lateinit var signUpButton : Button
+    lateinit var noteRepository: NoteRepository
+    lateinit var emailAddressEditText: EditText
+    lateinit var confirmedPasswordEditText: EditText
+    lateinit var initialPasswordET: EditText
+    lateinit var signUpButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
         init()
     }
-    fun init(){
+
+    fun init() {
 
         noteRepository = NoteRepository(this)
         noteRepository.getAllNotes()
@@ -30,16 +33,27 @@ class SignUp : AppCompatActivity() {
         signUpButton = findViewById(R.id.register_button)
 
         signUpButton.setOnClickListener {
-            if (initialPasswordET != confirmedPasswordEditText){
-                Toast.makeText(this, "Password mismatched", Toast.LENGTH_SHORT).show()
+//            if (initialPasswordET == confirmedPasswordEditText) {
+                saveEntry()
+                val intent = Intent(this, Login::class.java)
+                startActivity(intent)
+
+        //}else{
+//
+//                Toast.makeText(this, "Password mismatched", Toast.LENGTH_SHORT).show()
             }
+  //      }
+
+    }
+
+    fun saveEntry() {
+        if (emailAddressEditText.text.isNullOrBlank() || confirmedPasswordEditText.text.isNullOrBlank()) {
+            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show()
+        } else {
+            val note = NoteEntity(emailAddressID = emailAddressEditText.text.toString(), passwordDescription = confirmedPasswordEditText.text.toString())
+            noteRepository.insertNote(note)
+            finish()
         }
 
     }
-
-    fun saveEntry(){
-        if (emailAddressEditText.text.isNullOrBlank() && confirmedPasswordEditText.text.isNullOrBlank())
-            Toast.makeText(this, "All fields are required", Toast.LENGTH_SHORT).show()
-    }
-
 }
